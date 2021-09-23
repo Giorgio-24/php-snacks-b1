c473273f2cc62ef37ba43ac93fdecebc9e69b571
Giorgio Andrei <cipuandrei@libero.it>
2021-09-23 16:07:03 +0200

    function-for-different-random-numbers-created

---
+++ snack-1/data.php
@@ -0,0 +1,20 @@
+<?php
+$lowest = 1;
+$highest = 100;
+$total = 15;
+function get_random_numbers($min, $max, $quantity)
+{
+    $numbers = [];
+    for ($i = 0; $i < $quantity; $i++) {
+        $random = rand($min, $max);
+        if (!in_array($random, $numbers)) {
+            array_push($numbers, $random);
+        } else {
+            $i--;
+        };
+    };
+    return $numbers;
+};
+
+$my_numbers = get_random_numbers($lowest, $highest, $total);
+var_dump($my_numbers);
