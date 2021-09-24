b28162018b74f5d9df878706f84103c5727ae18a
Giorgio Andrei <cipuandrei@libero.it>
2021-09-24 09:48:15 +0200

    function-done-with-while-cycle

--- snack-1/data.php
+++ snack-1/data.php
@@ -2,18 +2,18 @@
 $lowest = 1;
 $highest = 100;
 $total = 15;
+
 function get_random_numbers($min, $max, $quantity)
 {
     $numbers = [];
-    for ($i = 0; $i < $quantity; $i++) {
+
+    while (count($numbers) < $quantity) {
         $random = rand($min, $max);
-        if (!in_array($random, $numbers)) {
-            array_push($numbers, $random);
-        } else {
-            $i--;
-        };
+        if (!in_array($random, $numbers)) $numbers[] = $random;
     };
+
     return $numbers;
 };
+
 
 $my_numbers = get_random_numbers($lowest, $highest, $total);
